1d0fe6256d4f801352374213862c21156f7087c1
Nolan Platt <nolanplatt@nolan-macintosh.local>
2019-12-03 16:01:09 -0800

    push
    push

--- Uiportfolio/Control/reverse.c
+++
@@ -1,9 +0,0 @@
-//
-//  reverse.c
-//  Uiportfolio
-//
-//  Created by Nolan Platt on 12/3/19.
-//  Copyright © 2019 JM. All rights reserved.
-//
-
-#include <stdio.h>
---
+++ Uiportfolio/ViewControllers/ReverseController.m
@@ -0,0 +1,23 @@
+//
+//  ReverseController.m
+//  Uiportfolio
+//
+//  Created by Nolan Platt on 12/3/19.
+//  Copyright © 2019 JM. All rights reserved.
+//
+
+
+#import "ReverseController.h"
+#include "control.h"
+
+@implementation ReverseController
+@synthesize reverse;
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [reverse setText:@"0.0"];
+
+}
+
+@end
